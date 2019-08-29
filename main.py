6c6cb65bbff865ac58e998ff6c9e0cabdaebd5cd
cbrandt6 <cbrandt6@ycp.edu>
2019-08-28 23:27:39 -0400

    yeet

--- main.py
+++ main.py
@@ -4,4 +4,10 @@ import loginInfo
 
 
 def main():
-    
+    # Create chrome options to disable notifications
+    chrome_options = webdriver.ChromeOptions()
+    prefs = {"profile.default_content_setting_values.notifications": 2}
+    chrome_options.add_experimental_option("prefs", prefs)
+
+    # Get the browser and open it
+    browser = webdriver.Chrome(options=chrome_options)
