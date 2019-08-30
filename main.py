2afaabb91ec2901be7696330842eab8d76cb7fd2
cbrandt6 <cbrandt6@ycp.edu>
2019-08-29 20:58:21 -0400

    Send the browser to twitter
    Also added a way to quit the browser

--- main.py
+++ main.py
@@ -12,10 +12,25 @@ def main():
     # Get the browser and open it
     browser = webdriver.Chrome(options=chrome_options)
 
-    # TODO: Create a list of words that can be used to form a tweet
-    # TODO: Create a method of composing tweets
+    try:
 
-    browser.get("https://google.com")
+        # Send the bot to twitter
+        browser.get("https://twitter.com")
+
+        # TODO: Create a list of words that can be used to form a tweet
+        # TODO: Create a method of composing tweets
+        # Limit tweet to 240 chars
+
+    finally:
+        quitChars = ["q", "Q"]
+        quit = input("Enter Q to close browser")
+        while quit not in quitChars:
+            if quit == "q" or quit == "Q":
+                browser.quit()
+            else:
+                quit = input("Enter Q to close browser")
+
+
 
 
 main()
