63162124596e74829270fd5fa5766b33d1710fc3
cbrandt6 <cbrandt6@ycp.edu>
2019-09-18 19:33:26 -0400

    Move logic for posting tweet to tweeting function

--- main.py
+++ main.py
@@ -97,38 +97,7 @@ def main():
                 print("You must log in first!")
             # Only if the user is logged in and has a tweet composed, post it
             else:
-                # Present options to user
-                print("Your options are: ")
-                for i in range(people.__len__()):
-                    print(people[i])
-                # Get input from user
-                person = input("Who would you like to tweet as? ")
-                size = input("Tweet size? [s/m/l] ")
-                tweet = composetweet(person.lower(), size)
-                # Show the tweet to the user
-                print(tweet)
-                # Create a while loop that runs until the user either declines the tweet, or accepts a tweet
-                # Rerolling does not break the loop so the user can reroll until they are satisfied or change their mind
-                loop = True
-                while loop:
-                    # Ask the user to confirm that the tweet should be sent
-                    # The idea is for each tweet to be proofread so that nothing terrible is said
-                    confirmpost = input("Confirm tweet? [y/n/reroll] ")
-                    # If the user approves of the tweet, continue with posting and break the loop
-                    if confirmpost in affirmatives:
-                        # Post the tweet
-                        posttweet(tweet, browser)
-                        # End the loop
-                        loop = False
-                    # The user has elected to generate a new tweet
-                    elif confirmpost == "reroll":
-                        # Generate a new tweet using the same person
-                        tweet = composetweet(person.lower(), size)
-                        # Show newly generated tweet
-                        print(tweet)
-                    # The user has opted not to continue with the post. Breaks the loops and enters back into main loop
-                    else:
-                        loop = False
+                posttweet(browser)
         # Prompt user to make another selection, also giving them a chance to end the loop
         navigate = input("What would you like to do? (login, composetweet, scrape, posttweet) ")
     # Once the loop ends close the browser
@@ -226,17 +195,51 @@ def scrapeweets(person):
 
 
 # Posts a composed tweet to the bots twitter account
-def posttweet(tweet, browser):
-    # XPATH for draft box
-    # //*[@id="react-root"]/div/div/div/main/div/div/div/div[1]/div/div[2]/div[2]/div[1]/div/div/div[2]/div[1]/div/div/div/div/div/div/div/div/div[1]/div/div/div/div[2]/div
-    draft = browser.find_element_by_xpath("//*[@id=\"react-root\"]/div/div/div/main/div/div/div/div[1]/div/div[2]/div[2]/div[1]/div/div/div[2]/div[1]/div/div/div/div/div/div/div/div/div[1]/div/div/div/div[2]/div")
-    draft.send_keys(tweet)
+def posttweet(browser):
+    # Present options to user
+    print("Your options are: ")
+    for i in range(people.__len__()):
+        print(people[i])
+    # Get input from user
+    person = input("Who would you like to tweet as? ")
+    size = input("Tweet size? [s/m/l] ")
+    tweet = composetweet(person.lower(), size)
+    # Show the tweet to the user
+    print(tweet)
+    # Create a while loop that runs until the user either declines the tweet, or accepts a tweet
+    # Rerolling does not break the loop so the user can reroll until they are satisfied or change their mind
+    loop = True
+    while loop:
+        # Ask the user to confirm that the tweet should be sent
+        # The idea is for each tweet to be proofread so that nothing terrible is said
+        confirmpost = input("Confirm tweet? [y/n/reroll] ")
+        # If the user approves of the tweet, continue with posting and break the loop
+        if confirmpost in affirmatives:
+            # Post the tweet
+            # XPATH for draft box
+            # //*[@id="react-root"]/div/div/div/main/div/div/div/div[1]/div/div[2]/div[2]/div[1]/div/div/div[2]/div[1]/div/div/div/div/div/div/div/div/div[1]/div/div/div/div[2]/div
+            draft = browser.find_element_by_xpath(
+                "//*[@id=\"react-root\"]/div/div/div/main/div/div/div/div[1]/div/div[2]/div[2]/div[1]/div/div/div[2]/div[1]/div/div/div/div/div/div/div/div/div[1]/div/div/div/div[2]/div")
+            draft.send_keys(tweet)
 
-    # Post button xpath
-    # //*[@id="react-root"]/div/div/div/main/div/div/div/div[1]/div/div[2]/div[2]/div[1]/div/div/div[2]/div[2]/div/div/div[2]/div[3]/div/span/span
-    postbutton = browser.find_element_by_xpath("//*[@id=\"react-root\"]/div/div/div/main/div/div/div/div[1]/div/div[2]/div[2]/div[1]/div/div/div[2]/div[2]/div/div/div[2]/div[3]/div/span/span")
-    postbutton.click()
-    print("Tweet posted!")
+            # Post button xpath
+            # //*[@id="react-root"]/div/div/div/main/div/div/div/div[1]/div/div[2]/div[2]/div[1]/div/div/div[2]/div[2]/div/div/div[2]/div[3]/div/span/span
+            postbutton = browser.find_element_by_xpath(
+                "//*[@id=\"react-root\"]/div/div/div/main/div/div/div/div[1]/div/div[2]/div[2]/div[1]/div/div/div[2]/div[2]/div/div/div[2]/div[3]/div/span/span")
+            postbutton.click()
+            print("Tweet posted!")
+            # End the loop
+            loop = False
+        # The user has elected to generate a new tweet
+        elif confirmpost == "reroll":
+            # Generate a new tweet using the same person
+            tweet = composetweet(person.lower(), size)
+            # Show newly generated tweet
+            print(tweet)
+        # The user has opted not to continue with the post. Breaks the loops and enters back into main loop
+        else:
+            loop = False
+
 
 
 # Filters out periods, commas, links and other things
