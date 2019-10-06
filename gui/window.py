a4e38376c114eb42cd3d197d578ce4f32d641df4
cbrandt6 <cbrandt6@ycp.edu>
2019-10-06 19:16:05 -0400

    Basic BASIC gui. connected login
    Basically have to rewrite main but that is ok.

---
+++ gui/window.py
@@ -0,0 +1,43 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QPushButton, QVBoxLayout, QMainWindow
+import sys
+import functions
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.height = 300
+        self.width = 500
+        self.setGeometry(200, 200, self.width, self.height)
+        self.setWindowTitle("Twitbot")
+        self.initUI()
+
+    def initUI(self):
+        self.twitlabel = QLabel(self)
+        self.twitlabel.setText("Twitbot")
+        self.twitlabel.setGeometry(0, 0, 300, 100)
+        self.twitlabel.setStyleSheet("font: 30pt Arial; background-color: rgb(66, 135, 245)")
+        self.twitlabel.move((self.width / 2) - (self.twitlabel.width() / 2), (self.height / 4) - (self.twitlabel.height() / 2))
+        self.twitlabel.setAlignment(Qt.AlignCenter)
+        self.loginbutton = QPushButton(self)
+        self.loginbutton.setStyleSheet("font: 13pt Arial")
+        self.loginbutton.setGeometry(0, 0, 60, 40)
+        self.loginbutton.setText("Login")
+        self.loginbutton.move((self.width / 2) - (self.loginbutton.width() / 2), self.twitlabel.pos().y()+120)
+        # self.loginbutton.clicked.connect(functions.login.login)
+        self.scrapebutton= QPushButton(self)
+        self.scrapebutton.setText("Scrape")
+        self.scrapebutton.setGeometry(0, 0, 64, 40)
+        self.scrapebutton.setStyleSheet("font: 13pt Arial")
+        self.scrapebutton.move(self.loginbutton.pos().x() - 2, self.loginbutton.pos().y()+self.scrapebutton.height())
+        self.tweetbutton = QPushButton(self)
+        self.tweetbutton.setText("Post Tweet")
+        self.tweetbutton.setGeometry(0, 0, 88, 40)
+        self.tweetbutton.setStyleSheet("font: 13pt Arial")
+        self.tweetbutton.move(self.loginbutton.pos().x() - 14, self.scrapebutton.pos().y() + self.scrapebutton.height())
+
+    def update(self):
+        pass
+
+
--- main.py
+++ main.py
@@ -1,64 +1,38 @@
 from selenium import webdriver
 from functions import login, composetweet, posttweet, scrapetweets, gowild
+from gui import window
+import sys
+from PyQt5.QtWidgets import QApplication
+
 
 # Inputs to quit
 quitChars = ["q", "Q", "quit", "Quit"]
 
 
-# Main while loop that handles user inputs
-# Makes calls to other functions based on user inputs
-def main():
-    try:
-        loggedin = False
+class main():
+    def __init__(self):
+        # Create the browser
         # Create chrome options to disable notifications
         chrome_options = webdriver.ChromeOptions()
         prefs = {"profile.default_content_setting_values.notifications": 2}
         chrome_options.add_experimental_option("prefs", prefs)
 
         # Get the browser and open it
-        browser = webdriver.Chrome(options=chrome_options)
-        # If it can't for some reason find the window, use this
-        # window = browser.current_window_handle
-        # browser.switch_to.window(window)
+        self.browser = webdriver.Chrome(options=chrome_options)
 
-        # The user can use the console to to various things
-        navigate = input("What would you like to do? (login, composetweet, scrape, posttweet) ")
-        navigate = navigate.lower()
-        # Loop runs until user opts to quit
-        while navigate not in quitChars:
-            if navigate == "login":
-                login.login(browser)
-                loggedin = True
-            elif navigate == "composetweet":
-                tweet = composetweet.composetweet()
-                # Print the tweet for the person
-                print(tweet)
-            elif navigate == "scrape":
-                scrapetweets.scrapeweets()
-            # Ensure the user has logged in and a tweet has been composed first
-            elif navigate == "posttweet":
-                # Make sure the user is logged in
-                if not loggedin:
-                    print("You must log in first!")
-                # Only if the user is logged in and has a tweet composed, post it
-                else:
-                    posttweet.posttweet(browser)
-            elif navigate == "gowild":
-                try:
-                    # If the user enters a non int number it throws a value error exception
-                    delay = int(input("Time delay in seconds between posts: "))
-                    # Quickly ensure delay is an int
-                    gowild.gowild(browser, delay)
-                # Catch the exception and ask the user to enter an int next time
-                except ValueError:
-                    print("Please enter an integer value for the delay")
-            # Prompt user to make another selection, also giving them a chance to end the loop
-            navigate = input("What would you like to do? (login, composetweet, scrape, posttweet) ")
+        # Create the PyQt app and window
+        self.app = QApplication(sys.argv)
+        self.wind = window.MainWindow()
 
-    finally:
-        # Once the loop ends close the browser
-        if browser is not None:
-            browser.quit()
+        # Connect the functions to their calls from main
+        self.wind.loginbutton.clicked.connect(self.calllogin)
+
+        self.wind.show()
+        self.app.exec_()
+
+    def calllogin(self):
+        login.login(self.browser)
 
 
-main()
+
+main().__init__()
