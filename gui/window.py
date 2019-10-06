b637af78c1bce6a027a78af89d9113e7d59c2c93
cbrandt6 <cbrandt6@ycp.edu>
2019-10-06 19:41:42 -0400

    Closing the window now properly ends the function and closes the browser

--- gui/window.py
+++ gui/window.py
@@ -6,6 +6,7 @@ import functions
 
 class MainWindow(QMainWindow):
     def __init__(self):
+        # Create the main window
         super(MainWindow, self).__init__()
         self.height = 300
         self.width = 500
@@ -13,24 +14,28 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("Twitbot")
         self.initUI()
 
+    # Initialize the initial UI, or the main page
     def initUI(self):
+        # Main label
         self.twitlabel = QLabel(self)
         self.twitlabel.setText("Twitbot")
         self.twitlabel.setGeometry(0, 0, 300, 100)
         self.twitlabel.setStyleSheet("font: 30pt Arial; background-color: rgb(66, 135, 245)")
         self.twitlabel.move((self.width / 2) - (self.twitlabel.width() / 2), (self.height / 4) - (self.twitlabel.height() / 2))
         self.twitlabel.setAlignment(Qt.AlignCenter)
+        # Login button
         self.loginbutton = QPushButton(self)
         self.loginbutton.setStyleSheet("font: 13pt Arial")
         self.loginbutton.setGeometry(0, 0, 60, 40)
         self.loginbutton.setText("Login")
         self.loginbutton.move((self.width / 2) - (self.loginbutton.width() / 2), self.twitlabel.pos().y()+120)
-        # self.loginbutton.clicked.connect(functions.login.login)
+        # Scrape button
         self.scrapebutton= QPushButton(self)
         self.scrapebutton.setText("Scrape")
         self.scrapebutton.setGeometry(0, 0, 64, 40)
         self.scrapebutton.setStyleSheet("font: 13pt Arial")
         self.scrapebutton.move(self.loginbutton.pos().x() - 2, self.loginbutton.pos().y()+self.scrapebutton.height())
+        # Post tweet button
         self.tweetbutton = QPushButton(self)
         self.tweetbutton.setText("Post Tweet")
         self.tweetbutton.setGeometry(0, 0, 88, 40)
--- main.py
+++ main.py
@@ -9,7 +9,10 @@ from PyQt5.QtWidgets import QApplication
 quitChars = ["q", "Q", "quit", "Quit"]
 
 
-class main():
+# Main is now a class, as it should have been
+class TwitBot:
+    # The constructor for main now creates the browser, and also creates the display window
+    # Along with this, it also connects the functions that can be called to the buttons on the window
     def __init__(self):
         # Create the browser
         # Create chrome options to disable notifications
@@ -25,14 +28,37 @@ class main():
         self.wind = window.MainWindow()
 
         # Connect the functions to their calls from main
+        # It is easiest to just place each function in a wrapper function, and assign the wrapper to a button
+        # It makes it easier to pass parameters and such
         self.wind.loginbutton.clicked.connect(self.calllogin)
+        self.wind.scrapebutton.clicked.connect(self.callscrapetweets)
 
+        # Make sure to show the window
         self.wind.show()
-        self.app.exec_()
+        # Actually executes the gui
+        sys.exit(self.app.exec_())
 
     def calllogin(self):
         login.login(self.browser)
 
+    @staticmethod
+    def callcomposetweet():
+        composetweet.composetweet()
+
+    def callposttweet(self):
+        posttweet.posttweet(self.browser)
+
+    @staticmethod
+    def callscrapetweets():
+        scrapetweets.scrapeweets()
 
 
-main().__init__()
+def main():
+    bot = TwitBot()
+    try:
+        bot.__init__()
+    finally:
+        bot.browser.close()
+
+if __name__ == "__main__":
+    main()
